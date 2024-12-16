717a0120b2a261505f2a58b9875c8ec723f8d11b
Guilherme Lopes <guilherme.lopes@dcx.ufpb.br>
2024-12-16 14:49:35 -0300

    Add files via upload

---
+++ Problema1004.java
@@ -0,0 +1,14 @@
+package beecrowd;
+
+import java.util.Scanner;
+
+public class Problema1004 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("PROD = " + (a * b));
+    }
+}

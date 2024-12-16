282c46a897a352726706b5822fee895c107833b2
Guilherme Lopes <guilherme.lopes@dcx.ufpb.br>
2024-12-16 14:44:09 -0300

    Add files via upload

---
+++ Problema1003.java
@@ -0,0 +1,14 @@
+package beecrowd;
+
+import java.util.Scanner;
+
+public class Problema1003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("SOMA = " + (a + b));
+    }
+}
